7317ca1d9185fa950b268c9d791be30a2b38fe1d
¨Pranav¨ <pranav.nakate@gmail.com>
2016-04-25 03:51:16 -0400

    Issues resolved in this commit: 1. FSRReader is transformed into a class with a main() function 2. Read/write API Keys of ThingSpeak are global variables 3. Periodic sampling (average of last 'n' readings) is implemented and tested. 4. Two new parameters 'num_observations' and 'sleep_interval' added for periodic sampling.
    To do:
    1. Read the ALERT signal of ADS1115 into a pin of Pi and use it to create an alert on ThingSpeak, may be by updating a field of the FSR channel.

--- FSRReader.py
+++ FSRReader.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python
+# -*- coding: UTF-8 -*-
 
 from __future__ import division
 import time
 import Adafruit_ADS1x15
 import math
+import RPi.GPIO as GPIO
 
 from ThingSpeak import ThingSpeak
-
-adc = Adafruit_ADS1x15.ADS1115()
 
 # Gain (multiplier) -- keep this 1
 GAIN = 2 
@@ -25,35 +25,55 @@ circuit_resistance = 20000
 # max value of ADC
 max_adc = 32767
 
-CUR = adc.start_adc_comparator(CHANNEL,
+# for periodic sampling
+num_observations = 5
+sleep_interval = 5 # in seconds
+
+class FSR_ADC:
+    def __init__(self):
+        # instantiate the ADC library
+        self.adc = Adafruit_ADS1x15.ADS1115()
+
+        # for integration with ThingSpeak
+        self.thingSpeak = ThingSpeak()
+
+    def read_FSR(self):
+        CUR = self.adc.start_adc_comparator(CHANNEL,
                                HT, LT,
                                gain = GAIN,
                                traditional = True,
                                active_low = True,
                                num_readings = 1,
                                latching = True)
+        i = 0
+        val_array = []
 
-i = 0
-# integration with ThingSpeak
-thingSpeak = ThingSpeak()
+        while True:
+            cur_value = self.adc.get_last_result()
+            cur_force = self.get_force(cur_value)
 
-def get_force(adc_value):
-    cur_resistance = circuit_resistance * cur_value / (max_adc - circuit_resistance)
-    cur_force = ((math.log(144000000 / cur_resistance)) * math.log(100))/ math.log(24000)
-    return math.pow(cur_force, math.e)
+            val_array.append(cur_force)
+            #print ('Current channel {} value is {} = {} grams; at iteration {}'.format(CHANNEL, cur_value, cur_force, i))
+            i += 1
 
-while True:
-    cur_value = adc.get_last_result()
-    cur_force = get_force(cur_value)
+            if i >= num_observations:
+                avg_force = float(sum(val_array)) / len(val_array)
+                #print "Average force = " + str(avg_force)
+                val_array = []
+                i = 0
 
-    print ('Current channel {} value is {} = {} grams; at iteration {}'.format(CHANNEL, cur_value, cur_force, i))
-    i += 1
-    if i > 5:
-        break
+            # updating the ThingSpeak channel
+            #print thingSpeak.update_fsr_channel(cur_force)
 
-    # updating the ThingSpeak channel
-    print thingSpeak.update_fsr_channel(cur_force)
+            time.sleep(sleep_interval)
 
-    time.sleep(4)
+        self.adc.stop_adc()
 
-adc.stop_adc()
+    def get_force(self,adc_value):
+        cur_resistance = circuit_resistance * adc_value / (max_adc - circuit_resistance)
+        cur_force = ((math.log(144000000 / cur_resistance)) * math.log(100))/ math.log(24000)
+        return math.pow(cur_force, math.e)
+
+if __name__== "__main__":
+    fsr = FSR_ADC()
+    fsr.read_FSR()
--- ThingSpeak.py
+++ ThingSpeak.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 
 import urllib2
 
 hostname = "https://api.thingspeak.com"
 
+writeAPIKey = "3XICIX7N986U4JAS"
+readAPIKey = None
+
+#TODO -- should have a config file to read channel numbers, read/write API Keys (ThingSpeak) and field ids
+
 class ThingSpeak:
 
     def __init__(self):
         self.hostname = hostname
-        
+
+        # ADD key-value pairs to this map to update other ThingSpeak fields
+        self.fields = {"FSR":1}
+    
     def getLastFeed(self, readAPIKey, channelId, fieldId):
-        url = self.hostname + "/channels/" + channelId + "/field/" \
-              + fieldId + "/last?key=" + readAPIKey
+        url = self.hostname + "/channels/" + channelId + "/field/" + fieldId + "/last?key=" + readAPIKey
         return urllib2.urlopen(url)
     
     def updateChannel(self, writeAPIKey, fieldId, value):
@@ -19,14 +27,12 @@ class ThingSpeak:
         return urllib2.urlopen(url)
 
     def update_fsr_channel(self, value):
-        writeAPIKey = "3XICIX7N986U4JAS"
-        fieldID = 1
+        fieldID = self.fields.get("FSR")
         return self.updateChannel(writeAPIKey, fieldID, value).read()
 
+# FOR testing
 if __name__=="__main__":
-    #curhost = "https://api.thingspeak.com"
-    
     thingSpeak = ThingSpeak()
-    
-    print thingSpeak.updateChannel("PHTKY9VQ9FIEYRWB", "1", "8888").read()
-    print thingSpeak.getLastFeed("QOT8G5E9VF7YW10S", "108160", "1").read()
+    #print thingSpeak.updateChannel("PHTKY9VQ9FIEYRWB", "1", "8888").read()
+    #print thingSpeak.getLastFeed("QOT8G5E9VF7YW10S", "108160", "1").read()
+    thingSpeak.update_fsr_channel(24)
